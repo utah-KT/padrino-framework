2d2c86cc28c738e48c7e5932d207d4a537f51780
ファム ヴァンフン <vanhung.pham@mixi.co.jp>
2018-11-07 17:56:20 +0900

    fix loader

--- padrino-core/lib/padrino-core/loader.rb
+++ padrino-core/lib/padrino-core/loader.rb
@@ -55,7 +55,7 @@ module Padrino
       Padrino.logger
       Reloader.lock!
       before_load.each(&:call)
-      dependency_paths.each{ |path| require_dependencies(path) }
+      require_dependencies(*dependency_paths)
       after_load.each(&:call)
       logger.devel "Loaded Padrino in #{Time.now - began_at} seconds"
       Thread.current[:padrino_loaded] = true
